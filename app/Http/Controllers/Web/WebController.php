43d05f569018bb9fb053b129f221828d5dace41e
renato-Aspire-E5-574 <remontanari@live.com>
2022-12-20 14:55:35 -0300

    routes update

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -104,79 +104,8 @@ class WebController extends Controller
         ]);
     }
 
-    public function artigo(Request $request)
-    {
-        $post = Post::where('slug', $request->slug)->postson()->first();
-        
-        $categorias = CatPost::orderBy('titulo', 'ASC')
-            ->where('tipo', 'artigo')
-            ->get();
-        $postsMais = Post::orderBy('views', 'DESC')
-            ->where('id', '!=', $post->id)
-            ->where('tipo', 'artigo')
-            ->limit(4)
-            ->postson()
-            ->get();
-        $postsTags = Post::orderBy('views', 'DESC')
-            ->where('tipo', 'artigo')
-            ->where('tags', '!=', '')
-            ->where('id', '!=', $post->id)
-            ->postson()
-            ->limit(11)
-            ->get();
-        
-        $post->views = $post->views + 1;
-        $post->save();
+       
 
-        $head = $this->seo->render($post->titulo ?? 'Informática Livre',
-            $post->titulo,
-            route('web.blog.artigo', ['slug' => $post->slug]),
-            $post->cover() ?? $this->configService->getConfig()->getMetaImg()
-        );
-
-        return view('web.'.$this->configService->getConfig()->template.'.blog.artigo', [
-            'head' => $head,
-            'post' => $post,
-            'postsMais' => $postsMais,
-            'postsTags' => $postsTags,
-            'categorias' => $categorias
-        ]);
-    }    
-
-    public function pesquisa(Request $request)
-    {
-        $search = $request->only('search');
-
-        $paginas = Post::where(function($query) use ($request){
-            if($request->search){
-                $query->orWhere('titulo', 'LIKE', "%{$request->search}%")
-                    ->where('tipo', 'pagina')->postson();
-                $query->orWhere('content', 'LIKE', "%{$request->search}%")
-                    ->where('tipo', 'pagina')->postson();
-            }
-        })->postson()->limit(10)->get();
-
-        $artigos = Post::where(function($query) use ($request){
-            if($request->search){
-                $query->orWhere('titulo', 'LIKE', "%{$request->search}%")
-                    ->where('tipo', 'artigo')->postson();
-                $query->orWhere('content', 'LIKE', "%{$request->search}%")
-                    ->where('tipo', 'artigo')->postson();
-            }
-        })->postson()->limit(10)->get();
-        
-        $head = $this->seo->render('Pesquisa por ' . $request->search ?? 'Informática Livre',
-            'Pesquisa - ' . $this->configService->getConfig()->nomedosite,
-            route('web.blog.artigos'),
-            $this->configService->getConfig()->getMetaImg() ?? 'https://informaticalivre.com/media/metaimg.jpg'
-        );
-        
-        return view('web.'.$this->configService->getConfig()->template.'.pesquisa',[
-            'head' => $head,
-            'paginas' => $paginas,
-            'artigos' => $artigos
-        ]);
-    }
 
     public function pagina($slug)
     {
@@ -211,91 +140,8 @@ class WebController extends Controller
         ]);
     }
 
-    public function acomodacoes()
-    {
-        $acomodacoes = Apartamento::available()->get();
-        $head = $this->seo->render('Acomodações - ' . $this->configService->getConfig()->nomedosite,
-            $this->configService->getConfig()->descricao ?? 'Informática Livre desenvolvimento de sistemas web desde 2005',
-            route('web.acomodacoes'),
-            $this->configService->getConfig()->getMetaImg() ?? 'https://informaticalivre.com/media/metaimg.jpg'
-        );
-        return view('web.'.$this->configService->getConfig()->template.'.acomodacoes.index',[
-            'head' => $head,
-            'acomodacoes' => $acomodacoes
-        ]);
-    }
 
-    public function reservar(Request $request)
-    {
-        $dadosForm = $request->all();
-        $acomodacoes = Apartamento::available()->get();
 
-        $paginareserva = Post::where('id', 15)->first();
-        $politicareserva = Post::where('id', 14)->first();
-        $paginareserva->views = $paginareserva->views + 1;
-        $paginareserva->save();
-
-        $head = $this->seo->render('Pré-reserva - ' . $this->configService->getConfig()->nomedosite,
-            'Pré-reserva - ' . $this->configService->getConfig()->nomedosite,
-            route('web.reservar'),
-            $this->configService->getConfig()->getMetaImg() ?? 'https://informaticalivre.com/media/metaimg.jpg'
-        );
-        
-        return view('web.'.$this->configService->getConfig()->template.'.acomodacoes.reservar',[
-            'head' => $head,
-            'dadosForm' => $dadosForm,
-            'acomodacoes' => $acomodacoes,
-            'paginareserva' => $paginareserva,
-            'politicareserva' => $politicareserva,
-            'estados' => $this->estadoService->getEstados()
-        ]);
-    }
-
-    public function acomodacao($slug)
-    {
-        $acomodacao = Apartamento::where('slug', $slug)->available()->first();
-        $acomodacoes = Apartamento::where('id', '!=', $acomodacao->id)->available()->get();
-
-        $postsTags = Post::orderBy('views', 'DESC')
-            ->where('tags', '!=', '')
-            ->where('id', '!=', $acomodacao->id)
-            ->postson()
-            ->limit(11)
-            ->get();
-
-        $acomodacao->views = $acomodacao->views + 1;
-        $acomodacao->save();
-
-        $paginareserva = Post::where('id', 5)->first();
-        $paginareserva->views = $paginareserva->views + 1;
-        $paginareserva->save();
-
-        $head = $this->seo->render($acomodacao->titulo . ' - ' . $this->configService->getConfig()->nomedosite,
-            $acomodacao->descricao ?? 'Informática Livre desenvolvimento de sistemas web desde 2005',
-            route('web.acomodacao', ['slug' => $acomodacao->slug]),
-            $this->configService->getConfig()->getMetaImg() ?? 'https://informaticalivre.com/media/metaimg.jpg'
-        );
-        return view('web.'.$this->configService->getConfig()->template.'.acomodacoes.acomodacao',[
-            'head' => $head,
-            'acomodacao' => $acomodacao,
-            'acomodacoes' => $acomodacoes,
-            'postsTags' => $postsTags,
-            'estados' => $this->estadoService->getEstados()
-        ]);
-    }
-
-    public function avaliacaoCliente(Request $request)
-    {
-        $head = $this->seo->render('Questionário de avaliação - ' . $this->configService->getConfig()->nomedosite,
-            'Questionário de avaliação',
-            route('web.avaliacao'),
-            $this->configService->getConfig()->getMetaImg() ?? 'https://informaticalivre.com/media/metaimg.jpg'
-        );
-
-        return view('web.'.$this->configService->getConfig()->template.'.cliente.avaliacao',[
-            'head' => $head
-        ]);
-    }
 
     public function sitemap()
     {
@@ -334,12 +180,5 @@ class WebController extends Controller
             'galeria' => $galeria,
             'head' => $head
         ]);
-    }
- 
-    public function zapchat(Request $request)
-    {
-        $textoZap = $request->texto;
-        $link = \App\Helpers\WhatsApp::getNumZap($this->configService->getConfig()->whatsapp, $textoZap);
-        return redirect($link);
-    }
+    } 
 }
