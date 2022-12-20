da6c43f2ccfac462bd62fa0334f7e105012f22d5
renato-Aspire-E5-574 <remontanari@live.com>
2022-12-20 17:04:45 -0300

    create Arquivo MVC

---
+++ database/migrations/2022_12_20_170000_create_arquivos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArquivosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('arquivos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('arquivos');
+    }
+}
